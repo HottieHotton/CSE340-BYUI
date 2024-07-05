08648b1d5de843eeae9d729ff9b3deb70c338d64
Braxton Hotton <bhotton25@gmail.com>
2024-07-04 22:23:10 -0600

    Finished learning activity

---
+++ routes/accountRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
+const utilities = require("../utilities/");
+
+router.get("/login", accountController.buildLogin);
+// Process the login attempt
+router.post("/login", accountController.buildLogin);
+
+router.get("/register", accountController.buildRegister);
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,10 +8,15 @@
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
+const bodyParser = require("body-parser")
 const app = express()
+const session = require("express-session");
+const pool = require('./database/');
 const static = require("./routes/static.js")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute.js")
+const accountRoute = require("./routes/accountRoute.js")
+const accountController = require("./controllers/accountController.js");
 const utilities = require("./utilities/index.js");
 
 /* ***********************
@@ -21,12 +26,33 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout.ejs")
 
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get("/", utilities.handleErrors(baseController.buildHome))
-app.use("/inv", inventoryRoute)
+app.use("/inv", utilities.handleErrors(inventoryRoute))
+app.use("/account", require("./routes/accountRoute"))
+app.get("/login", utilities.handleErrors(accountController.buildLogin))
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
